629a2ed44afe7003d5f1e045c8da402502b9bb1a
Sergey Blagodarin <s.blagodarin@gmail.com>
2016-03-27 12:37:28 +0300

    Improved command line processing.

--- src/main.cpp
+++ src/main.cpp
@@ -2,19 +2,42 @@
 #include "minidump.h"
 #include "processor.h"
 #include <iostream>
+#include <boost/optional/optional.hpp>
+#include <boost/program_options/parsers.hpp>
+#include <boost/program_options/positional_options.hpp>
+#include <boost/program_options/variables_map.hpp>
 
 int main(int argc, char** argv)
 {
-	if (argc != 2 && argc != 3)
+	std::string dump_name;
+	boost::optional<std::string> commands;
+	try
 	{
-		std::cerr << "Usage:\n\t" << argv[0] << " DUMP [COMMAND]" << std::endl;
+		boost::program_options::options_description o;
+		o.add_options()
+			("dump", boost::program_options::value<std::string>()->required())
+			("commands", boost::program_options::value<std::string>());
+
+		boost::program_options::positional_options_description p;
+		p.add("dump", 1).add("commands", 1);
+
+		boost::program_options::variables_map vm;
+		boost::program_options::store(boost::program_options::command_line_parser(argc, argv).options(o).positional(p).run(), vm);
+		boost::program_options::notify(vm);
+		dump_name = vm["dump"].as<std::string>();
+		if (vm.count("commands"))
+			commands = vm["commands"].as<std::string>();
+	}
+	catch (const boost::program_options::error&)
+	{
+		std::cerr << "Usage:\n\twhydebug DUMP [COMMAND]" << std::endl;
 		return 1;
 	}
 
 	std::unique_ptr<Minidump> dump;
 	try
 	{
-		dump = std::make_unique<Minidump>(argv[1]);
+		dump = std::make_unique<Minidump>(dump_name);
 	}
 	catch (const BadCheck& e)
 	{
@@ -23,8 +46,8 @@ int main(int argc, char** argv)
 	}
 
 	Processor processor(std::move(dump));
-	if (argc == 3)
-		return processor.process(argv[2]) ? 0 : 1;
+	if (commands)
+		return processor.process(*commands) ? 0 : 1;
 	for (std::string line; ; )
 	{
 		std::cout << "?> ";
